8f1d708d18f19d6d63ef899959973f12abee82d6
Shivam <shivam.s72@kalvium.community>
2025-09-21 18:43:07 +0530

    done

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'drizzle-kit';
-import type { Config } from 'drizzle-kit';
+import 'dotenv/config'
 
-const dbConfig: Config = defineConfig({
+export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'turso',
+  dialect: 'sqlite',
   dbCredentials: {
     url: process.env.TURSO_CONNECTION_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
-});
-
-export default dbConfig;
+}
